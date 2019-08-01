e2749bd0b59f3e012d4188980b8054db3a248517
guyaqi <guyaqi5858@outlook.com>
2019-08-02 04:40:27 +0800

    add trace

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function queryDo (query, callback) {
     let el = document.querySelector(query)
     if (query!==null) {
@@ -22,3 +24,31 @@ queryListen('#bell', 'click', ()=> {
         window.open(url1);
     }
 })
+
+queryListen('#bell', 'click', ()=> {
+    let url1 = "http://wpa.qq.com/msgrd?v=3&uin=1360668777&site=oicqzone.com&menu=yes";
+    let url2 = "mqqwpa://im/chat?chat_type=wpa&uin=1360668777&version=1&src_type=web&web_src=oicqzone.com";
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) || /(Android)/i.test(navigator.userAgent)) {
+
+        window.open(url2);
+    }
+    else {
+        window.open(url1);
+    }
+})
+
+queryListen('#bell', 'mouseenter', (e)=> {
+    let el = e.target
+    console.log(e)
+    el.style.position = 'absolute'
+    el.style.top = e.clientY - 40 + 'px'
+    el.style.left = e.clientX - 40 + 'px'
+
+    window.addEventListener('mousemove', (eIn)=> {
+        el.style.top = eIn.clientY - 40 + 'px'
+        el.style.left = eIn.clientX - 40 + 'px'
+    })
+})
+
+
+// #bell
