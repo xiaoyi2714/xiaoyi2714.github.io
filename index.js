55619c9866321b48f72ae8be2710791c048bf134
guyaqi <guyaqi5858@outlook.com>
2019-08-03 00:24:59 +0800

    end

--- index.js
+++ index.js
@@ -13,42 +13,25 @@ function queryListen (query, eventName, listener) {
     })
 }
 
-queryListen('#bell', 'click', ()=> {
-    let url1 = "http://wpa.qq.com/msgrd?v=3&uin=1360668777&site=oicqzone.com&menu=yes";
-    let url2 = "mqqwpa://im/chat?chat_type=wpa&uin=1360668777&version=1&src_type=web&web_src=oicqzone.com";
+function findQQ (qqnumber) {
+    let url1 = "http://wpa.qq.com/msgrd?v=3&uin="+qqnumber+"&site=oicqzone.com&menu=yes";
+    let url2 = "mqqwpa://im/chat?chat_type=wpa&uin="+qqnumber+"&version=1&src_type=web&web_src=oicqzone.com";
     if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) || /(Android)/i.test(navigator.userAgent)) {
-
         window.open(url2);
     }
     else {
         window.open(url1);
     }
+}
+
+function findMe () {
+    findQQ(1360668777)
+}
+
+queryListen('#action-findMe', 'click', ()=> {
+    findMe()
 })
 
-queryListen('#bell', 'click', ()=> {
-    let url1 = "http://wpa.qq.com/msgrd?v=3&uin=1360668777&site=oicqzone.com&menu=yes";
-    let url2 = "mqqwpa://im/chat?chat_type=wpa&uin=1360668777&version=1&src_type=web&web_src=oicqzone.com";
-    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) || /(Android)/i.test(navigator.userAgent)) {
-
-        window.open(url2);
-    }
-    else {
-        window.open(url1);
-    }
+queryListen('#action-goto-Gallery', 'click', ()=> {
+    window.location.href = 'gallery.html'
 })
-
-// queryListen('#bell', 'mouseenter', (e)=> {
-//     let el = e.target
-//     console.log(e)
-//     el.style.position = 'absolute'
-//     el.style.top = e.clientY - 40 + 'px'
-//     el.style.left = e.clientX - 40 + 'px'
-
-//     window.addEventListener('mousemove', (eIn)=> {
-//         el.style.top = eIn.clientY - 40 + 'px'
-//         el.style.left = eIn.clientX - 40 + 'px'
-//     })
-// })
-
-
-// #bell
