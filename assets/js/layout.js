bbb901dd33581c9be498d6c758a2af6a17493e61
guyaqi <guyaqi5858@qq.com>
2019-09-15 16:24:19 +0800

    fixed height calc

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -3,31 +3,37 @@ const claw = document.querySelector('#claw');
 // const blog = document.querySelector('#blog');
 
 let hide = true;
+let init = false;
 
 function layout() {
-    let width = document.body.clientWidth;
-    let height = document.body.clientHeight;
+    let width = document.documentElement.clientWidth ;
+    let height = document.documentElement.clientHeight ;
     let portrait = height > width;
 
     // disable transition before adjust
-    console.log('layout called');
+    // console.log('layout called');
 
     if(portrait) {
         hideSidebar();
         sidebar.style.width = '100%';
         blog.style.marginLeft = '0px';
-        sidebar.addEventListener('click', hideSidebar);
-        claw.style.display = 'block';
+        // console.log("P:"+height);
         claw.style.bottom = `${height/2 - 32}px`;
-        claw.addEventListener('click', toggleSidebar);
+        claw.style.display = 'block';
+        if(!init) {
+            init = true;
+            sidebar.addEventListener('click', hideSidebar);
+            claw.addEventListener('click', toggleSidebar);
+        }
+
     }
     else {
         showSidebar();
         sidebar.style.width = '200px';
         blog.style.marginLeft = '200px';
-        sidebar.removeEventListener('click', hideSidebar);
         claw.style.display = 'none';
-        claw.removeEventListener('click', toggleSidebar);
+
+        // console.log("L:"+height);
     }
 
     // enable transition after adjust
